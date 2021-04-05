b88c9970051070d6101c89bcd61cf08e622759e7
Clementwdk <wodkowskiclement@orange.fr>
2021-04-05 22:17:34 +0100

    Add models Users and create new account

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 
@@ -7,6 +7,16 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///app.db"
 
 db = SQLAlchemy(app)
+
+class Usercon(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    countryFav = [db.Column('CRestrict')]
+
+    def __init__(self, name, password):
+        self.name = name
+        self.password = password
 
 class CRestrict(db.Model):
     country = db.Column(db.String(200), primary_key=True)
@@ -54,7 +64,6 @@ def home():
         userChoice = request.form.get("ct")
         if request.form.get("subButton") == "Search":
             print("uuuu "+userChoice)
-            #url_for('http://127.0.0.1:5000/res', country=userChoice)
             return redirect(url_for('res', country=userChoice))
 
     return render_template('Index.html', country=country)
@@ -85,8 +94,28 @@ def countryList():
 def logIn():
     return render_template('logIn.html')
 
+@app.route('/nwaccount', methods=['POST', 'GET'])
+def nwaccount():
+    if request.method == "POST":
+        user = request.form.get("nlog")
+        pwd = request.form.get("npwd")
+        if not user:
+            flash("Username empty")
+        elif not pwd:
+            flash("Password empty")
+        else:
+            try:
+                db.session.add(Usercon(user,pwd))
+                print(1)
+            except:
+                return 'Impossible to creat a new account'
+            flash("Create Succesful")
+
+    return render_template('nwaccount.html')
+
 
 if __name__ == "__main__":
+    app.secret_key = 'super secret key'
     app.run(debug=True)
 
 
